8fca2586e9e0e645f248f7d809c7ccd730063383
ohnaka0410 <>
2021-01-07 00:41:33 +0900

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+/**
+ * ESLint Configuration
+ */
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    // prettierと重複するルールを無効化
+    "prettier/@typescript-eslint",
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    project: "./tsconfig.eslint.json",
+  },
+  "env": { "browser": true, "node": true, "es6": true },
+  "rules": {
+    // custom
+  }
+};
